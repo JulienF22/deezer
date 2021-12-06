d4c7cac45960fe564b243b3c0728ea70b9a98ffd
Julien Faure <julien.faure@orange.com>
2021-12-06 16:57:05 +0100

    add nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './component/album/album.component';
+import { FormulaireComponent } from './component/formulaire/formulaire.component';
+import { TestComponent } from './component/test/test.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'form', component: FormulaireComponent },
+  { path: 'test', component: TestComponent },
+  { path: '', redirectTo: '/form', pathMatch: 'full' },
+  { path: '**', redirectTo: '/form', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,9 @@ import { AlbumComponent } from './component/album/album.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormulaireComponent } from './component/formulaire/formulaire.component';
 import { FormsModule } from '@angular/forms';
+import { TestComponent } from './component/test/test.component';
 @NgModule({
-  declarations: [AppComponent, AlbumComponent, FormulaireComponent],
+  declarations: [AppComponent, AlbumComponent, FormulaireComponent, TestComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
